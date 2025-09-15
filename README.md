1368251880c3147042c4a090cedbf67a7df116f9
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-09-15 18:36:12 +0200

    starting load data for neuropix

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # allen-data
 set of analysis scripts related to data from the Allen brain institute
+
+## Installation & Environments
+
+```
+brew install hdf5 # or install hdf5 support
+```
+
+### Neuropixels Visual Coding
+
+```
+conda create -n "npxallen" python=3.9
+conda activate npxallen
+pip install allensdk
+```
---
+++ neuropixels-visual-coding/Load-Data.py
@@ -0,0 +1,11 @@
+# %%
+import os
+from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
+manifest_path=os.path.join(os.path.expanduser('~'), 'Downloads', 'ecephys_cache_dir', 'manifest.json')
+cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
+sessions = cache.get_session_table()
+
+print('Total number of sessions: ' + str(len(sessions)))
+
+sessions.head()
+# %%
