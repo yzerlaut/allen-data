938b627404c04b39008e8b57935e73f5d0bd93fc
Yann Zerlaut <yann.zerlaut@gmail.com>
2025-09-15 19:42:49 +0200

    adding npx draft

--- neuropixels-visual-coding/Load-Data.py
+++ neuropixels-visual-coding/Load-Data.py
@@ -1,8 +1,12 @@
 # %%
 import os
-from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
-manifest_path=os.path.join(os.path.expanduser('~'), 'Downloads', 'ecephys_cache_dir', 'manifest.json')
-cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
+from allensdk.brain_observatory.ecephys.ecephys_project_cache\
+      import EcephysProjectCache
+manifest_path=os.path.join(\
+                os.path.expanduser('~'), 'Downloads',
+                'ecephys_cache_dir', 'manifest.json')
+cache = EcephysProjectCache.from_warehouse(\
+                                manifest=manifest_path)
 sessions = cache.get_session_table()
 
 print('Total number of sessions: ' + str(len(sessions)))
